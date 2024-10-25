3124aa0e8b8b5aa2d9b7e5f482c45a4a059cd40b
GiuliaMoiraghi10 <gmoiraghi10@gmail.com>
2024-10-25 16:23:55 +0200

    Snack2

--- script.js
+++ script.js
@@ -63,6 +63,69 @@ console.log (`La bici ${kgBici.nome} è la più leggera e pesa ${kgBici.kg} kg`)
 
 
 
+/* Snack2
+Creare un array di oggetti di squadre di calcio.
+ Ogni squadra avrà diverse proprietà: nome, punti fatti, falli subiti.
+Nome sarà l’unica proprietà da compilare, le altre saranno tutte settate a 0.
+Generare numeri random al posto degli 0 nelle proprietà “punti” fatti e “falli subiti”.
+Infine, creiamo un nuovo array i cui elementi contengono solo nomi e falli subiti e stampiamo tutto in console.
+*/
+
+// creare array di oggetti
+//  - proprietà: nome, punti fatti, falli subiti (tutte a 0 tranne nome)
+ const squadre = [
+    {
+        nome: 'Inter',
+        puntiFatti: 0,
+        falliSubiti: 0
+    },
+
+    {
+        nome: 'Milan',
+        puntiFatti: 0,
+        falliSubiti: 0
+    },
+
+    {
+        nome: 'Juve',
+        puntiFatti: 0,
+        falliSubiti: 0
+    },
+
+    {
+        nome: 'Roma',
+        puntiFatti: 0,
+        falliSubiti: 0
+    },
+
+    {
+        nome: 'Atalanta',
+        puntiFatti: 0,
+        falliSubiti: 0
+    }
+
+ ]
+
+//  console.log (squadre)
+
+ // creo ciclo for per assegnare numeri random
+
+const datiSquadra = []
+
+for (let i = 0; i < squadre.length; i++) {
+  const totSquadre = squadre[i]
+  totSquadre.falliSubiti = (Math.floor(Math.random()*50)+1)
+  totSquadre.puntiFatti = (Math.floor(Math.random()*50)+1)
+
+  console.log (totSquadre)
+  console.log("Punti fatti:",totSquadre.puntiFatti)
+  console.log("Falli subiti:",totSquadre.falliSubiti)
+
+  datiSquadra.push(totSquadre.nome)
+  datiSquadra.push(totSquadre.falliSubiti)
+  console.log("Nome e falli subiti:",datiSquadra)
+}
+
 
 
 
